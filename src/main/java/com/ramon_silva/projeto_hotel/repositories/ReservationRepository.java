a0b1d1f4c62c0d06b65504eec5d9cd28fa3acc95
Ramon-Wesley <ramonwj.s@outlook.com>
2023-07-29 20:07:14 -0300

    adicao e modificacao de repositories

--- src/main/java/com/ramon_silva/projeto_hotel/repositories/ReservationRepository.java
+++ src/main/java/com/ramon_silva/projeto_hotel/repositories/ReservationRepository.java
@@ -1,8 +1,15 @@
 package com.ramon_silva.projeto_hotel.repositories;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.ramon_silva.projeto_hotel.models.ReservationModel;
 
-public interface ReservationRepository extends JpaRepository<ReservationModel,Long>{
-}
+public interface ReservationRepository extends JpaRepository<ReservationModel, Long> {
+    
+   
+ }
